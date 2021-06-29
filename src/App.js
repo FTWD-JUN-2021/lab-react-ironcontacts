af2edae32e6b14d9e1081bd0a2cd080b8482902d
leannewilson <83836254+leannewilson@users.noreply.github.com>
2021-06-29 14:06:28 -0700

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import contacts from './contacts';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts";
+import { useState } from "react";
 
 function App() {
-  const [list, setList] = useState(contacts.splice(0,5));
+  const [list, setList] = useState(contacts.splice(0, 5));
+  const [rest, setRest] = useState(contacts);
 
   function displayList() {
     return list.map((contact) => {
       return (
         <tr>
           <td>
-            <img 
-              src={contact.pictureUrl}
-              style={{ width: "8vw" }} 
-            />
+            <img src={contact.pictureUrl} style={{ width: "8vw" }} />
           </td>
-          <td><h5>{contact.name}</h5></td>
-          <td><h5>{contact.popularity.toFixed(2)}</h5></td>
+          <td>
+            <h5>{contact.name}</h5>
+          </td>
+          <td>
+            <h5>{contact.popularity.toFixed(2)}</h5>
+          </td>
         </tr>
-      )
-    })
+      );
+    });
   }
 
+  function addRandomContact() {
+    let restOfContacts = [...rest];
+    let copyOfList = [...list];
+    let randomIndex = Math.floor(Math.random() * restOfContacts.length);
+    let random = restOfContacts[randomIndex];
+    copyOfList.unshift(random);
+    setList(copyOfList);
+    restOfContacts.splice(randomIndex, 1);
+    setRest(restOfContacts);
+  }
 
   console.log(list);
   return (
     <div className="App">
       <table>
-      <h2>IronContact</h2>
+        <h2>IronContact</h2>
+        <button onClick={addRandomContact}>Add Random Contact</button>
         <tr>
           <th>Picture</th>
           <th>Name</th>
